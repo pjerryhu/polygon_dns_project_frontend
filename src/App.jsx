58d18cd5762dbbd082f06aedb4302022512eba14
husjerry <3605555+pjerryhu@users.noreply.github.com>
2022-02-19 23:32:35 +0000

    use newest contract

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 // Add the domain you will be minting
 const tld = '.dc';
-const CONTRACT_ADDRESS = '0x6bc4Df6766670cE801a83AC852254c30e7013f76';
+const CONTRACT_ADDRESS = '0xd48Abf1E97c63B9fda98f6d2a4C43F93ce4Bc370';
 
 const App = () => {
   	//Just a state variable we use to store our user's public wallet. Don't forget to import useState at the top.
@@ -24,11 +24,6 @@ const App = () => {
   const [domain, setDomain] = useState('');
   const [record, setRecord] = useState('');
   const [network, setNetwork] = useState('');
-  // Add a new stateful variable at the start of our component next to all the old ones
-	const [editing, setEditing] = useState(false);
-  const [mints, setMints] = useState([]);
-
-
 
 
 	// Implement your connectWallet method here
@@ -129,115 +124,52 @@ const App = () => {
 
 	};
 
- const mintDomain = async () => {
-	// Don't run if the domain is empty
-	if (!domain) { return }
-	// Alert the user if the domain is too short
-	if (domain.length < 3) {
-		alert('Domain must be at least 3 characters long');
-		return;
-	}
-	// Calculate price based on length of domain (change this to match your contract)	
-	// 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
-	const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
-	console.log("Minting domain", domain, "with price", price);
-	try {
-    	const { ethereum } = window;
-    	if (ethereum) {
-			const provider = new ethers.providers.Web3Provider(ethereum);
-			const signer = provider.getSigner();
-			const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
-
-			console.log("Going to pop wallet now to pay gas...")
-      		let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
-      		// Wait for the transaction to be mined
-			const receipt = await tx.wait();
-
-			// Check if the transaction was successfully completed
-			if (receipt.status === 1) {
-				console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
-				
-				// Set the record for the domain
-				tx = contract.setRecord(domain, record);
-				await tx.wait();
-
-				console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
-				
-				// Call fetchMints after 2 seconds
-				setTimeout(() => {
-					fetchMints();
-				}, 2000);
-
-				setRecord('');
-				setDomain('');
-			} else {
-				alert("Transaction failed! Please try again");
-			}
-    	}
-  	} catch(error) {
-    	console.log(error);
-  	}
-}
-
-
-  const updateDomain = async () => {
-    if (!record || !domain) { return }
-    setLoading(true);
-    console.log("Updating domain", domain, "with record", record);
-      try {
-      const { ethereum } = window;
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
-
-        let tx = await contract.setRecord(domain, record);
-        await tx.wait();
-        console.log("Record set https://mumbai.polygonscan.com/tx/"+tx.hash);
-
-        fetchMints();
-        setRecord('');
-        setDomain('');
-      }
-      } catch(error) {
-        console.log(error);
-      }
-    setLoading(false);
-  }
-
-  const fetchMints = async () => {
+  const mintDomain = async () => {
+    // Don't run if the domain is empty
+    if (!domain) { return }
+    // Alert the user if the domain is too short
+    if (domain.length < 3) {
+      alert('Domain must be at least 3 characters long');
+      return;
+    }
+    // Calculate price based on length of domain (change this to match your contract)	
+    // 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+    const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
+    console.log("Minting domain", domain, "with price", price);
     try {
       const { ethereum } = window;
       if (ethereum) {
-        // You know all this
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
-          
-        // Get all the domain names from our contract
-        const names = await contract.getAllNames();
-          
-        // For each name, get the record and the address
-        const mintRecords = await Promise.all(names.map(async (name) => {
-        const mintRecord = await contract.records(name);
-        const owner = await contract.domains(name);
-        return {
-          id: names.indexOf(name),
-          name: name,
-          record: mintRecord,
-          owner: owner,
-        };
-      }));
 
-      console.log("MINTS FETCHED ", mintRecords);
-      setMints(mintRecords);
+        console.log("Going to pop wallet now to pay gas...")
+        let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+        // Wait for the transaction to be mined
+        const receipt = await tx.wait();
+
+        // Check if the transaction was successfully completed
+        if (receipt.status === 1) {
+          console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+          
+          // Set the record for the domain
+          tx = contract.setRecord(domain, record);
+          await tx.wait();
+
+          console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+          
+          setRecord('');
+          setDomain('');
+        }
+        else {
+          alert("Transaction failed! Please try again");
+        }
       }
-    } catch(error){
+    }
+    catch(error){
       console.log(error);
     }
   }
-
-
 
 
 	// Create a function to render if wallet is not connected yet
@@ -252,50 +184,16 @@ const App = () => {
   
 	// Form to enter domain name and data
 	const renderInputForm = () =>{
-    		if (network !== 'Polygon Mumbai Testnet') {
-			return (
-				<div className="connect-wallet-container">
-					<p>Please connect to Polygon Mumbai Testnet</p>
-				</div>
-			);
-		}
-    / Add this render function next to your other render functions
-const renderMints = () => {
-	if (currentAccount && mints.length > 0) {
-		return (
-			<div className="mint-container">
-				<p className="subtitle"> Recently minted domains!</p>
-				<div className="mint-list">
-					{ mints.map((mint, index) => {
-						return (
-							<div className="mint-item" key={index}>
-								<div className='mint-row'>
-									<a className="link" href={`https://testnets.opensea.io/assets/mumbai/${CONTRACT_ADDRESS}/${mint.id}`} target="_blank" rel="noopener noreferrer">
-										<p className="underlined">{' '}{mint.name}{tld}{' '}</p>
-									</a>
-									{/* If mint.owner is currentAccount, add an "edit" button*/}
-									{ mint.owner.toLowerCase() === currentAccount.toLowerCase() ?
-										<button className="edit-button" onClick={() => editRecord(mint.name)}>
-											<img className="edit-icon" src="https://img.icons8.com/metro/26/000000/pencil.png" alt="Edit button" />
-										</button>
-										:
-										null
-									}
-								</div>
-					<p> {mint.record} </p>
-				</div>)
-				})}
-			</div>
-		</div>);
-	}
-};
+    if (network !== 'Polygon Mumbai Testnet') {
+      return (
+        <div className="connect-wallet-container">
+          <h2>Please switch to Polygon Mumbai Testnet</h2>
+          {/* This button will call our switch network function */}
+          <button className='cta-button mint-button' onClick={switchNetwork}>Click here to switch</button>
+        </div>
+      );
+    }
 
-// This will take us into edit mode and show us the edit buttons!
-const editRecord = (name) => {
-	console.log("Editing record for", name);
-	setEditing(true);
-	setDomain(name);
-}
 
 		return (
 			<div className="form-container">
@@ -312,27 +210,16 @@ const editRecord = (name) => {
 				<input
 					type="text"
 					value={record}
-					placeholder='whats ur ninja power?'
+					placeholder='whats ur ninja power'
 					onChange={e => setRecord(e.target.value)}
 				/>
-					{/* If the editing variable is true, return the "Set record" and "Cancel" button */}
-					{editing ? (
-						<div className="button-container">
-							// This will call the updateDomain function we just made
-							<button className='cta-button mint-button' disabled={loading} onClick={updateDomain}>
-								Set record
-							</button>  
-							// This will let us get out of editing mode by setting editing to false
-							<button className='cta-button mint-button' onClick={() => {setEditing(false)}}>
-								Cancel
-							</button>  
-						</div>
-					) : (
-						// If editing is not true, the mint button will be returned instead
-						<button className='cta-button mint-button' disabled={loading} onClick={mintDomain}>
-							Mint
-						</button>  
-					)}
+
+				<div className="button-container">
+					<button className='cta-button mint-button' onClick={mintDomain}>
+						Mint
+					</button>  
+				</div>
+
 			</div>
 		);
 	}
@@ -341,13 +228,6 @@ const editRecord = (name) => {
 	useEffect(() => {
 		checkIfWalletIsConnected();
 	}, [])
-  
-  useEffect(() => {
-    if (network === 'Polygon Mumbai Testnet') {
-      fetchMints();
-    }
-  }, [currentAccount, network]);
-
 
   return (
 		<div className="App">
@@ -369,9 +249,8 @@ const editRecord = (name) => {
 
         				
 				{!currentAccount && renderNotConnectedContainer()}
-        {currentAccount && renderInputForm()}
-        {mints && renderMints()}
-
+				{/* Render the input form if an account is connected */}
+				{currentAccount && renderInputForm()}
 
 
 
